bb2a8fbb125c48a2e0919db00504455b09c90fe6
Vivek <itspersonal4vivek@gmail.com>
2024-04-02 00:40:33 +0530

    has created Dogcard and has implemented various ways to use it

---
+++ src/DogCard.js
@@ -0,0 +1,58 @@
+// function Image()
+// {
+//     return <img src="https://as1.ftcdn.net/v2/jpg/05/59/27/48/1000_F_559274893_O9iSRQwTKIkAooNTglilMgx2yMcXK9Or.jpg"/>
+
+// }
+
+// function DogCard(){
+//     return( 
+//     <div> 
+//         <h3>Dog</h3>
+//        <Image/>
+       
+//     </div>
+//     )
+// }
+
+// export default DogCard;
+
+
+// export function Image()
+// {
+//     return <img src="https://as1.ftcdn.net/v2/jpg/05/59/27/48/1000_F_559274893_O9iSRQwTKIkAooNTglilMgx2yMcXK9Or.jpg"/>
+
+// }
+
+// export function DogCard(){
+//     return( 
+//     <div> 
+//         <h3>Dog</h3>
+//        <Image/>
+       
+//     </div>
+//     )
+// }
+
+import Image  from "./Image"
+
+//  function DogCard(){
+//     return( 
+//     <div> 
+//         <h3>Dog</h3>
+//        <Image/>
+       
+//     </div>
+//     )
+// }
+
+//  what if we don't want to  return as wrapping div
+function DogCard(){
+    return(
+        <>
+        <h3>Dog</h3>
+        <Image/>
+        </>
+    )
+}
+
+export default DogCard
